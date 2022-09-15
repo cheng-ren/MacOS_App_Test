fc4a642bee115b8341fe66b6bbddad9ec1207480
任成 <rencheng11@icloud.com>
2022-09-15 20:39:13 +0800

    add auto update framework

---
+++ MacOS_App_Test/MacOS_App_Test/Updater.swift
@@ -0,0 +1,16 @@
+//
+//  Updater.swift
+//  MacOS_App_Test
+//
+//  Created by 任成 on 2022/9/15.
+//
+
+import Foundation
+import AppKit
+
+class Updater: NSObject {
+    
+    @IBAction func checkForUpdates(_ object: AnyObject) {
+        
+    }
+}
